2bb17e4b4e76879c4a884c55a819b83f67df9b5f
SanchezSDario <dariosebastiansanchez@gmail.com>
2020-09-02 19:28:27 -0300

    Creado proyecto base y configurado gradle, agregue los packages base tambien.

---
+++ src/main/java/ar/edu/unq/desapp/GrupoJ022020/desappapl/DesappAplApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.desapp.GrupoJ022020.desappapl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesappAplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesappAplApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unq/desapp/GrupoJ022020/desappapl/DesappAplApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.desapp.GrupoJ022020.desappapl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesappAplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
