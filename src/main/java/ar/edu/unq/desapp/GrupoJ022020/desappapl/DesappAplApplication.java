a05b5d93fee3bc96e7f249d69d6d9a718fc869a3
Jenami <jimenarosato@gmail.com>
2020-09-12 19:18:42 -0300

    heroku test1

--- src/main/java/ar/edu/unq/desapp/GrupoJ022020/desappapl/DesappAplApplication.java
+++ src/main/java/ar/edu/unq/desapp/GrupoJ022020/desappapl/DesappAplApplication.java
@@ -2,9 +2,18 @@ package ar.edu.unq.desapp.GrupoJ022020.desappapl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
 
+@Controller
 @SpringBootApplication
 public class DesappAplApplication {
+
+ @RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "Hello World!";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(DesappAplApplication.class, args);
